d0c0b057d7f81f59d0e53b68647dac55b3ba8239
bGG178 <bowersgrant13@gmail.com>
2025-02-10 20:59:44 -0700

    Data Processing Function Complete
    Data process function complete, returns dictionary containing min, max, average and median values

--- stocks.py
+++ stocks.py
@@ -1,8 +1,9 @@
 
-from numpy.ma.core import empty
+from numpy.ma.core import empty, floor
 from requests import get
 import json
 from datetime import date
+from math import ceil
 
 def download_data(ticker: str) -> dict:
     ticker = ticker.upper()
@@ -19,17 +20,62 @@ def download_data(ticker: str) -> dict:
     except Exception as e:
         print(f"Exception occurred {e}")
 
+def data_process(data : dict) -> dict:
+    closingprices =[]
+    largestprice = 0
+    smallestprice = 99999999
+
+    processeddict = {'min':None,'max':None,'avg':None,'median':None}
+    for price in list(data["data"]["tradesTable"]["rows"]):
+        pricetoint = float(price["close"].replace('$',''))
+        closingprices.append(pricetoint)
+
+        if (pricetoint > largestprice):
+            largestprice = pricetoint
+        if (pricetoint < smallestprice):
+            smallestprice = pricetoint
+
+    #calculate average closing price
+    lengthprices = len(closingprices)
+    total = 0
+    for price in closingprices:
+        total += price
+    average = (total)/lengthprices
+    average = round(average,2)
+    processeddict['avg'] = average
+
+    #min and max
+    processeddict['min'],processeddict['max'] = smallestprice,largestprice
+
+
+    #median
+    closingprices.sort()
+    #Even length for list
+    if (lengthprices/2)%2 == 1:
+        #print((lengthprices // 2) - 1)
+        #print((lengthprices//2))
+        #print(closingprices[(lengthprices//2)-1])
+        #print(closingprices[(lengthprices//2)])
+
+        medianprice = (closingprices[(lengthprices//2)-1] + closingprices[int((lengthprices // 2))])/2
+
+    #Odd length for list
+    else:
+        medianprice = closingprices[(lengthprices//2)]
+
+    processeddict['median'] = medianprice
+    print(processeddict)
+
+    return processeddict
 
 data = download_data("TSLA")
-stockfile = "stocks.json"
+stockfile = "rawdata.json"
+processfile = "stocks.json"
 
 with open(stockfile, "w") as file:
     json.dump(data, file)
 
-for price in list(data["data"]["tradesTable"]["rows"]):
-    print("Date: " + price["date"])
-    print("Opening: " + price["open"])
-    print("Close: " +price["close"])
-    print("Highest: " + price["high"])
-    print("Lowest: " + price["low"])
-    print()
+postprocess = data_process(data)
+
+with open(processfile, "w") as file:
+    json.dump(postprocess,file)
